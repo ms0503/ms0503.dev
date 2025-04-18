2ae6e766bd412eea4a45472476cb40a06d035833
Sora Tonami <ms0503@outlook.com>
2025-04-18 14:20:36 +0900

    feature: ブログ本文のMDX・GFMサポートを追加。

--- app/routes/blog-post.tsx
+++ app/routes/blog-post.tsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import { getPostBodyById, getPostById } from '~/models/post';
+import { mdxToReact } from '~/services/doc-convert.server';
 import type { Route } from './+types/blog-post';
 
 export default function BlogPost({ loaderData: { body, post } }: Route.ComponentProps) {
@@ -14,7 +15,7 @@ export default function BlogPost({ loaderData: { body, post } }: Route.Component
 
 export async function loader({ context: { db }, params: { postId } }: Route.LoaderArgs) {
     const post = await getPostById(db, postId);
-    const body = await getPostBodyById(db, postId);
+    const body = await getPostBodyById(db, postId).then(raw => mdxToReact(raw));
     return {
         body,
         post
