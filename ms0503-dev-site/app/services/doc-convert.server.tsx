b5bf729de0b0ffd10e874cef90e485b062b20733
Sora Tonami <ms0503@outlook.com>
2025-07-04 17:16:46 +0900

    feature: site: リンクをLinkタグへ書き換える処理を実装

--- ms0503-dev-site/app/services/doc-convert.server.tsx
+++ ms0503-dev-site/app/services/doc-convert.server.tsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import production from 'react/jsx-runtime';
+import { Link } from 'react-router';
 import rehypeRaw from 'rehype-raw';
 import rehypeReact from 'rehype-react';
 import remarkBreaks from 'remark-breaks';
@@ -9,7 +10,9 @@ import remarkMdx from 'remark-mdx';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import { unified } from 'unified';
-import type { JSX } from 'react';
+import type {
+    AnchorHTMLAttributes, JSX, PropsWithChildren
+} from 'react';
 
 export async function mdxToReact(mdx: string): Promise<JSX.Element> {
     return unified()
@@ -19,7 +22,27 @@ export async function mdxToReact(mdx: string): Promise<JSX.Element> {
         .use(remarkGfm)
         .use(remarkRehype)
         .use(rehypeRaw)
-        .use(rehypeReact, production)
+        .use(rehypeReact, {
+            ...production,
+            components: {
+                a: LinkWrapper
+            }
+        })
         .process(mdx)
         .then((v: { result: JSX.Element }) => v.result);
+}
+
+function LinkWrapper({
+    children,
+    href
+}: PropsWithChildren<AnchorHTMLAttributes<HTMLAnchorElement>>) {
+    return href === '' || href?.startsWith('/') || href?.startsWith('#') ? (
+        <Link to={href}>
+            {children}
+        </Link>
+    ) : (
+        <a href={href} rel="noopener noreferrer" target="_blank">
+            {children}
+        </a>
+    );
 }
